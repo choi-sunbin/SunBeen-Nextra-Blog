60bac905755cf834934c947fbcdd5d64269dda8c
최선빈 <csn1434@naver.com>
2025-05-22 14:55:08 +0900

    default mode : Dark & add Gray-d

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,8 @@ const withNextra = nextra({
 export default withNextra({
 	images: {
 		domains: ["img.icons8.com"] // 허용할 외부 이미지 도메인 추가
-	}
+	},
+	darkMode: true, // 다크 모드 활성화
+	defaultTheme: "dark" // 기본 테마를 다크로 설정
 	// ... Add regular Next.js options here
 });
