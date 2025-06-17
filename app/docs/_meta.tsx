bd2068c3cbd4b8681d72bcbe19f8627efa9dab3e
Sunbin Choi <csn1434@naver.com>
2025-06-17 23:23:54 +0900

    FEAT: Kubernates Basic POST Why Kubernates & VM vs Container

--- app/docs/_meta.tsx
+++ app/docs/_meta.tsx
@@ -1,6 +1,7 @@
 export default {
   Docker: "Docker",
-  Kubernetes: "Kubernetes",
+  Kubernates_Basic: "Kubernates 기초",
+  Kubernates_Advenced: "Kubernetes Another Class",
   Typescript: "구 Typescript",
   Javascript: "구 Javascript",
 };
